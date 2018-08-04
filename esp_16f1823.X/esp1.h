6fb4dbfb7d4ded0aabedac8a1e1ee8b5adda928c
saul <saul@example.com>
2018-08-05 01:48:22 +0200

    added esp_16f1823

---
+++ esp_16f1823.X/esp1.h
@@ -0,0 +1,124 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_ESP1_H
+#define	XC_ESP1_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+// TODO Insert appropriate #include <>
+
+// TODO Insert C++ class definitions if appropriate
+
+// TODO Insert declarations
+typedef enum
+{
+    ESP_OTHER = 0,
+    ESP_SEND_OK,
+    ESP_OK,
+    ESP_EOL,
+    ESP_RX,
+    ESP_PROMPT,
+    ESP_CONNECT,
+    ESP_SEND_FAIL,
+    ESP_CLOSED,
+    ESP_ERROR
+} esp_mess;
+
+
+
+#define ESP_BUFFER_SIZE 16
+
+//#define ESP_TX_BUFFER_SIZE 64
+
+extern volatile unsigned char esp_channel;
+extern volatile unsigned char ESP_wait_exception;
+
+//extern volatile unsigned char ESP_tx_buf[ESP_TX_BUFFER_SIZE];
+//extern volatile unsigned char ESP_tx_buf_ind;
+
+
+
+// Comment a function and leverage automatic documentation with slash star star
+/**
+    <p><b>Function prototype:</b></p>
+  
+    <p><b>Summary:</b></p>
+
+    <p><b>Description:</b></p>
+
+    <p><b>Precondition:</b></p>
+
+    <p><b>Parameters:</b></p>
+
+    <p><b>Returns:</b></p>
+
+    <p><b>Example:</b></p>
+    <code>
+ 
+    </code>
+
+    <p><b>Remarks:</b></p>
+ */
+// TODO Insert declarations or function prototypes (right here) to leverage 
+// live documentation
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+unsigned char ESP_read(void);
+
+unsigned char ESP_process_message(void);
+
+void ESP_initialize(void);
+
+void ESP_config(void);
+
+void ESP_wait_for(unsigned char esp_mess);
+
+void ESP_message_handler(void);
+
+void ESP_write(unsigned char *pt, unsigned char lenght);
+
+void ESP_default_application_handler(unsigned char data);
+    
+void ESP_Set_application_handler(void (* InterruptHandler)(unsigned char));
+
+void ESP_process_rx_data(void);
+    
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
---
+++ esp_16f1823.X/logger.c
@@ -0,0 +1,204 @@
+#include "logger.h"
+#include "mcc_generated_files/mcc.h"
+#include "esp1.h"
+#include "comm.h"
+
+volatile unsigned char message_format;
+
+volatile unsigned short ADC_value;
+volatile unsigned char ADC_state;
+//volatile unsigned char ADC_count;
+
+volatile unsigned char TMR1_state;
+
+volatile unsigned char IOC_state;
+volatile unsigned char IOC_value;
+
+void logger_initialize(void)
+{
+    message_format = MESSAGE_BINARY; //ADC transmitted values default to binary
+      
+    ADC_SelectChannel(channel_AN2);       
+    ADC_state = ADC_IDLE;
+    //ADC_value = 0;
+    //ADC_count = 0;
+    
+    TMR1_StopTimer();
+    TMR1_state = TMR1_STOP;
+    TMR1_SetInterruptHandler(_TMR1_Ready); //Redirect TMR1_ISR_handler to custom function
+   
+    //IOCA4_SetInterruptHandler()
+    IOCAF4_SetInterruptHandler(_IOC_Ready); //Redirect IOC in A4 to custom function
+    IOC_state = IOC_IDLE;
+    IOC_value = 1; // R4 has pull-up resistor enabled;
+    
+    ESP_Set_application_handler(process_message); // Redirect ESP RX messages to logger process_message(unsigned char);
+}
+
+void bt_message_handler(void)
+{
+    unsigned char message;
+    message = EUSART_Read();
+    process_message(message);
+}
+
+void process_message(unsigned char message)
+{
+       
+    //Message handler
+    switch (message) {
+        case 't': //Test communications
+            #ifdef BT
+            _puts("Ok\n");            
+            #else
+            ESP_write("Ok\n",3);
+            #endif
+            break; 
+            
+        case 'a': //Ask analog value             
+            ADC_value = ADC_GetConversion(channel_AN2); 
+            read_analog();
+            break;
+            
+        case 'T': //toggle between binary data or text mode           
+            toggle_format();
+            break;
+            
+        case 's': //start continuous analog monitoring
+            if (TMR1_state == TMR1_RUNNING)
+                return;                     
+            TMR1_StartTimer();
+            TMR1_state = TMR1_RUNNING;
+            
+            break;
+            
+        case 'S': //stop continuous analog sampling              
+                    
+            TMR1_StopTimer();
+            TMR1_state = TMR1_STOP;
+            break;
+                           
+        case 'o': // Change digital output to low 
+            
+            IO_RA5_SetLow();
+            break;
+            
+        case 'O': // Change digital output to high
+            
+            IO_RA5_SetHigh();
+            break;
+                      
+        default:
+            break;            
+    }
+}
+
+void read_analog()
+{
+    unsigned short adc_val;
+    //unsigned char aux[2];
+    unsigned long  aux1;
+    unsigned char mess[6];
+      
+    adc_val = ADC_value;
+    //ADC_state = ADC_IDLE; //Reset flag!
+    
+    if (message_format == MESSAGE_BINARY) {    
+        mess[0] = (unsigned char)(adc_val & 0xff);
+        mess[1] = (unsigned char)((adc_val >> 8) & 0xff);
+    
+        #ifdef BT
+        write((unsigned char*)mess,2);
+        #else   
+     
+        /*
+        ESP_tx_buf[ESP_tx_buf_ind++] = mess[0];
+        ESP_tx_buf[ESP_tx_buf_ind++] = mess[1];
+        
+        if (ESP_tx_buf_ind == ESP_TX_BUFFER_SIZE) {
+            ESP_write(ESP_tx_buf,ESP_TX_BUFFER_SIZE);
+            ESP_wait_for(ESP_SEND_OK);
+            ESP_tx_buf_ind = 0;
+        } 
+        */
+        
+        ESP_write(mess,2);
+        ESP_wait_for(ESP_SEND_OK);
+        #endif
+    
+    } else {
+        #ifdef BT   
+            //val_mv = ((unsigned long)adc_val*3300UL/1024UL); 
+            aux1 = (unsigned long)adc_val*3300UL;
+            adc_val = (unsigned short)(aux1 >> 10); //divides by 1024. Result is in mV
+    
+            _sprintf(mess,adc_val); // Takes a value in mV and returns a string in V with 3 decimals
+            //mess[5] = '\n';
+        
+            _puts(mess);        
+            _puts("\n");
+        #else     
+            //mess[5] = '\n';
+            //ESP_write(mess,6);
+            //ESP_wait_for(ESP_SEND_OK);
+       
+        #endif
+        
+    }    
+    
+    ADC_state = ADC_IDLE; //Reset flag!
+}
+
+void toggle_format() 
+{
+    if (message_format == MESSAGE_BINARY) {
+        message_format = MESSAGE_TEXT;
+        #ifdef BT
+            _puts("tmode\n");
+        #else
+            ESP_write("tmode\n",6);
+        #endif
+    } else {
+        message_format = MESSAGE_BINARY;
+        #ifdef BT
+            _puts("bmode\n");
+        #else
+            ESP_write("bmode\n",6);
+        #endif
+    }
+}
+
+void _TMR1_Ready(void)
+{   
+    if (TMR1_state == TMR1_RUNNING) {
+                
+#ifdef BT
+        //if (ADC_state == ADC_IDLE) 
+        //    ADC_state = ADC_READY; 
+        ADC_StartConversion();
+#else
+        //if (ADC_state == ADC_IDLE) 
+        //    ADC_state = ADC_READY;         
+        ADC_StartConversion();
+#endif
+        
+        
+    }    
+}
+
+void _IOC_Ready(void)
+{
+    //__delay_ms(2); //Timeout for filter bouncing/noise    
+    IOC_value = IO_RA4_GetValue();    
+    IOC_state = IOC_READY;        
+}
+
+void process_ioc(void)
+{
+    IOC_state = IOC_IDLE;
+     if (IOC_value) {
+        _puts("CH");
+    } else {
+        _puts("CL");    
+    }
+}
