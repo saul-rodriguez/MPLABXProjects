d0b561a64093e882cbd50a10c077e25d6512d622
saul <saul@example.com>
2022-01-31 16:35:26 +0100

    created NMES_32 release

--- NMES_32.X/nbproject/Makefile-variables.mk
+++ NMES_32.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=NMES_32.X.production.null
-CND_ARTIFACT_PATH_default=dist/default/production/NMES_32.X.production.null
+CND_ARTIFACT_NAME_default=NMES_32.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/NMES_32.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=nmes32.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/nmes32.x.tar
---
+++ NMES_32.X/release/2022_01_31/__eeread.d
@@ -0,0 +1,2 @@
+dist/default/production/__eeread.p1: \
+  /opt/microchip/xc8/v2.31/pic/sources/c99/pic/__eeread.c
